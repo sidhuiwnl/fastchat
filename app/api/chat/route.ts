486443adb97bd5e4f009b090f6f84c746141534e
Sidharth <sidharthinfernal@gmail.com>
2025-06-09 00:19:45 +0530

    init

---
+++ app/api/chat/route.ts
@@ -0,0 +1,25 @@
+import { google } from '@ai-sdk/google';
+import { streamText } from 'ai';
+import {smoothStream} from "ai";
+
+
+export const maxDuration = 30;
+export const runtime = "edge";
+
+export async function POST(req: Request) {
+    const { messages } = await req.json();
+
+    const result = streamText({
+        model: google('gemini-2.0-flash'),
+        messages,
+        temperature : 0.7,
+        experimental_telemetry : {
+            isEnabled: false,
+        },
+        experimental_transform : smoothStream({
+            chunking : "line"
+        })
+    });
+
+    return result.toDataStreamResponse();
+}
